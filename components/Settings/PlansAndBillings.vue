e655fa05e012b89a7fa149ec85288fce0f3ce1a3
Dasaolu Daniel <yungdansa@gmail.com>
2023-04-23 12:58:06 +0000

    started settings flow

---
+++ components/Settings/PlansAndBillings.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="bg-white rounded-xl main">
+    <div class="flex h-16 w-full items-center justify-between px-6">
+      <h3 class="font-bold text-[1.125rem]">Organizations</h3>
+
+      <div class="text-sm flex font-medium items-center justify-end">
+        <span>Filter By</span>
+      </div>
+    </div>
+
+    <div>
+      <table class="table-auto w-full">
+        <thead class="w-full">
+          <tr>
+            <th
+              class="bg-[#f7f7f7] text-base font-medium px-6 py-4"
+              v-for="(header, index) in headers"
+            >
+              {{ header.title }}
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            v-for="(organization, index) in organizations"
+            :key="index"
+            class="cursor-pointer"
+            :class="index % 2 != 0 && 'bg-[#FCFCFE]'"
+          >
+            <td>
+            <nuxt-link :to="`ngos/${ organization.id}`"> 
+            {{ organization.name }}
+            </nuxt-link>
+            </td>
+            <td>{{ organization.plan }}</td>
+            <td>{{ organization.next_payment }}</td>
+            <td class="">{{ organization.amount }}</td>
+            <td> 
+            <span class="text-xs px-2 py-[.35rem] rounded-2xl capitalize" :class="organization.status == 'active' ? 'text-[#337138] bg-[#D1F7C4]' : 'text-[#3D435E] bg-[#E7EBF3]'"> {{ organization.status }} </span>
+            </td>
+
+           
+
+            <td> 
+                <Button
+                  :hasBorder="true"
+                  :hasIcon="false" 
+                  :text="organization.status == 'active' ? 'Cancel' : 'View'"
+                  :isGray="organization.status == 'active'"
+                  :disabled="organization.status == 'active'"
+                  class="text-[.875rem] !py-2 !px-3"
+                /> 
+            </td>
+ 
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const headers = ref([
+  { title: "Name" },
+  { title: "Plan" },
+  { title: "Next Payment" },
+  { title: "Amount" },
+  { title: "Status" },
+  { title: "Actions" },
+]);
+
+const organizations = ref([
+  {
+    id: "rf",
+    name: "test me",
+    plan: "tier 1",
+    next_payment: "Nov 30, 2020",
+    amount: "5,077",
+    status: "active",
+  }, 
+  {
+    id: "rf",
+    name: "test me",
+    plan: "tier 1",
+    next_payment: "Nov 30, 2020",
+    amount: "5,077",
+    status: "active",
+  }, 
+  {
+    id: "rf",
+    name: "test me",
+    plan: "tier 1",
+    next_payment: "Nov 30, 2020",
+    amount: "5,077",
+    status: "active",
+  }, 
+  {
+    id: "rf",
+    name: "test me",
+    plan: "tier 3",
+    next_payment: "Nov 30, 2020",
+    amount: "5,077",
+    status: "active",
+  }, 
+  {
+    id: "rf",
+    name: "test me",
+    plan: "tier 1",
+    next_payment: "Nov 30, 2020",
+    amount: "5,077",
+    status: "active",
+  }, 
+]);
+</script>
+
+<style lang="scss" scoped>
+.main {
+  box-shadow: 0px 3.17px 19.8125px rgba(174, 174, 192, 0.15);
+}
+table >tbody > tr> td  {
+  @apply align-middle text-center  text-base px-6 py-4 ;
+}
+</style>
---
+++ components/Settings/TwoFAuthentication.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="relative">
+    <div
+      v-for="(auth, index) in authentications"
+      :key="index"
+      class="my-5 rounded-lg bg-white p-5 ring-1 ring-primary-green"
+    >  
+      <div class="flex w-full items-center justify-between mb-4">
+        <div class="w-4/5">
+          <h2 class="font-bold mb-2">{{ auth.name }}</h2>
+          <p class="text-sm text-primary-gray">{{ auth.description }}</p>
+        </div>
+        
+        <label
+          class="my-toggle"
+          :for="auth.id"
+            >
+          <input
+            :id="auth.id" 
+            class="toggle-input"
+            type="checkbox" 
+          />
+          <div class="toggle-fill"></div>
+        </label>
+      </div>
+ 
+      <div 
+        class="mt-4 flex w-full items-center justify-between rounded-3xl bg-primary-light px-4 py-3 text-sm text-primary-gray"
+      >
+        <p>
+          <input 
+            disabled
+            type="checkbox"
+            class="mr-1 h-4 w-4 rounded-full text-primary-green"
+          />
+            Added {{ auth.date }}
+        </p>
+
+        <p class="flex items-center text-sm font-medium text-primary-black">
+          <span class="mr-2"> {{ auth.message }} :</span>
+          <span class="mr-2   uppercase">{{ auth.securityKey }}</span>
+            <WidgetsIconsEyes  title="open" />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const  authentications = ref([
+    {
+      name: "Google Authenticator",
+      id: "google-auth",
+      description:"Use the Google Authenticator app to generate a one time security codes",
+      date: "15 Dec, 2021",
+      message: "Back up code",
+      securityKey: "kakjdndjsnjns",
+      click: "click-function", 
+      showModal: false,
+      showSecurityDetails: false,
+      addedSecurity: false,
+    },
+    {
+      name: "Text Message (SMS)",
+      id: "text-message",
+      description: "Use the Google Authenticator app to generate a one time security codes",
+      date: "20 July, 2021",
+      message: "Binded phone number",
+      securityKey: "081354647373",
+      click: "click-function", 
+      showModal: false,
+      showSecurityDetails: false,
+      addedSecurity: true,
+    },
+  ],) 
+</script>
+
+<style></style>
